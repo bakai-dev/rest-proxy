e7fe6f0059c6ae73bc15a4daba5c66c6f11a738d
bakaiDev <developer.bakai@gmail.com>
2022-06-27 04:54:02 +0600

    Added comments | index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
@@ -6,13 +9,12 @@ use Symfony\Component\HttpFoundation\Request;
 use RestProxy\RestProxy;
 use RestProxy\CurlWrapper;
 
-/**
- * Set
- */
+
+// Init proxy
 $proxy = new RestProxy(Request::createFromGlobals(), new CurlWrapper());
-
+// Setup proxy URL
 $proxy->register('/', 'https://news.ycombinator.com');
-
+// Run proxy
 $proxy->run();
 
 echo $proxy->getContent();
